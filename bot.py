28f05fbb1c8d40cecc19dd703df6e6323dd8efc8
farzaneh2026 <farzanehghanfaziyan@gmail.com>
2026-07-23 16:51:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,8 @@ async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin.lower()}&vs_currencies=usd"
     data = requests.get(url).json()
-    price = float(data[coin.lower()]["usd"])
+    await update.message.reply_text(str(data))
+return
 
     tp1 = round(price * 1.015, 2)
     tp2 = round(price * 1.03, 2)
