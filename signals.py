a10f8ba3d900559e5a44daeed298879d4fb065d2
farzaneh2026 <farzanehghanfaziyan@gmail.com>
2026-07-24 22:58:06 +0330

    Update signals.py

--- signals.py
+++ signals.py
@@ -44,7 +44,7 @@ def get_signal():
             ema200 = df["ema200"].iloc[-1]
             rsi = df["rsi"].iloc[-1]
 
-            if price > ema50 and price > ema200 and rsi < 60:
+            if price > ema50 and price > ema200:
                 return {
                     "symbol": symbol,
                     "action": "BUY",
@@ -53,6 +53,15 @@ def get_signal():
                     "tp2": price * 1.04,
                     "sl": price * 0.98
                 }
+            elif price < ema50 and price < ema200:
+    return {
+        "symbol": symbol,
+        "action": "SELL",
+        "entry": price,
+        "tp1": price * 0.98,
+        "tp2": price * 0.96,
+        "sl": price * 1.02
+    
 
         except Exception:
             continue
