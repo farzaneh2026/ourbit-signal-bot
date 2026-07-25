61b6276528b425213afe29dff7f71e2382763d77
farzaneh2026 <farzanehghanfaziyan@gmail.com>
2026-07-25 23:01:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
-import requests
 
 from signals import get_signal, format_signal
+
 TOKEN = os.getenv("BOT_TOKEN")
+
+# آیدی تلگرام خودت را بعداً اینجا می‌گذاری
+CHAT_ID = os.getenv("CHAT_ID")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -14,28 +18,56 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/help - راهنما"
     )
 
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "این ربات سیگنال خرید و فروش را نمایش می‌دهد."
     )
 
+
 async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
     data = get_signal()
     text = format_signal(data)
-    await update.message.reply_text(text)
-    
-    
 
-    
+    await update.message.reply_text(text)
+
+
+# ارسال خودکار سیگنال
+async def auto_signal(context: ContextTypes.DEFAULT_TYPE):
+
+    if CHAT_ID:
+
+        data = get_signal()
+        text = format_signal(data)
+
+        await context.bot.send_message(
+            chat_id=CHAT_ID,
+            text=text
+        )
+
+
 def main():
+
     app = Application.builder().token(TOKEN).build()
+
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("signal", signal))
 
+
+    # هر ۱۵ دقیقه یک سیگنال خودکار
+    app.job_queue.run_repeating(
+        auto_signal,
+        interval=900,
+        first=10
+    )
+
+
     print("Bot Started...")
+
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
