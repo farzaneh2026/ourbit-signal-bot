af8fc8d419353ca8b89c22cdde457e079f4d254a
farzaneh2026 <farzanehghanfaziyan@gmail.com>
2026-07-25 20:40:41 +0330

    Update signals.py

--- signals.py
+++ signals.py
@@ -40,36 +40,65 @@ def get_signal():
                 window=50
             ).ema_indicator()
 
+            df["rsi"] = ta.momentum.RSIIndicator(
+                df["close"],
+                window=14
+            ).rsi()
+
+            macd = ta.trend.MACD(
+                df["close"]
+            )
+
+            df["macd"] = macd.macd()
+            df["macd_signal"] = macd.macd_signal()
+
             price = float(df["close"].iloc[-1])
             ema50 = float(df["ema50"].iloc[-1])
+            rsi = float(df["rsi"].iloc[-1])
+            macd_value = float(df["macd"].iloc[-1])
+            macd_signal = float(df["macd_signal"].iloc[-1])
 
-            distance = abs(price - ema50)
 
-            if price > ema50:
+            if price > ema50 and rsi < 70 and macd_value > macd_signal:
                 action = "BUY"
+
                 tp1 = round(price * 1.02, 2)
                 tp2 = round(price * 1.04, 2)
                 sl = round(price * 0.98, 2)
-            else:
+
+                strength = 3
+
+
+            elif price < ema50 and rsi > 30 and macd_value < macd_signal:
                 action = "SELL"
+
                 tp1 = round(price * 0.98, 2)
                 tp2 = round(price * 0.96, 2)
                 sl = round(price * 1.02, 2)
 
-            if best is None or distance > best["distance"]:
+                strength = 3
+
+            else:
+                continue
+
+
+            if best is None or strength > best["strength"]:
                 best = {
-                    "distance": distance,
+                    "strength": strength,
                     "symbol": symbol,
                     "action": action,
                     "entry": round(price, 2),
                     "tp1": tp1,
                     "tp2": tp2,
-                    "sl": sl
+                    "sl": sl,
+                    "rsi": round(rsi, 2)
                 }
 
+
         if best:
-            del best["distance"]
+            del best["strength"]
             return best
+
 
         return {
             "symbol": "NONE",
@@ -77,8 +106,10 @@ def get_signal():
             "entry": "-",
             "tp1": "-",
             "tp2": "-",
-            "sl": "-"
+            "sl": "-",
+            "rsi": "-"
         }
+
 
     except Exception as e:
         return {
@@ -87,8 +118,10 @@ def get_signal():
             "entry": str(e),
             "tp1": "-",
             "tp2": "-",
-            "sl": "-"
+            "sl": "-",
+            "rsi": "-"
         }
+
 
 
 def format_signal(signal):
@@ -101,6 +134,9 @@ def format_signal(signal):
 ✅ TP1: {signal['tp1']}
 ✅ TP2: {signal['tp2']}
 🛑 SL: {signal['sl']}
+
+📈 RSI: {signal['rsi']}
+📊 MACD: ✅
 
 ⚠️ معامله را دستی انجام بده.
 """
